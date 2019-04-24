98499c9d952b8f4c037e5d26901a415570db1286
TerrenceTakunda <ttmunyunguma@gmail.com>
2019-04-24 21:09:09 +0200

    Coleted schedules

---
+++ src/main/java/com/cuius/robotmind/CalendarView.java
@@ -0,0 +1,99 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.cuius.robotmind;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+
+/**
+ *
+ * @author terrence
+ */
+@Named(value = "calendarView")
+@SessionScoped
+public class CalendarView implements Serializable {
+
+    private Date date;
+    private Date time1;
+    private Date time2;
+    private Date time3;
+    private Date time4;
+    private Date time5;
+    /**
+     * Creates a new instance of CalendarView
+     */
+    public CalendarView() {
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Date getTime1() {
+        return time1;
+    }
+
+    public void setTime1(Date time1) {
+        this.time1 = time1;
+    }
+
+    public Date getTime2() {
+        return time2;
+    }
+
+    public void setTime2(Date time2) {
+        this.time2 = time2;
+    }
+
+    public Date getTime3() {
+        return time3;
+    }
+
+    public void setTime3(Date time3) {
+        this.time3 = time3;
+    }
+
+    public Date getTime4() {
+        return time4;
+    }
+
+    public void setTime4(Date time4) {
+        this.time4 = time4;
+    }
+
+    public Date getTime5() {
+        return time5;
+    }
+
+    public void setTime5(Date time5) {
+        this.time5 = time5;
+    }
+    
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+    }
+}
---
+++ src/main/java/com/cuius/robotmind/Schedules.java
@@ -0,0 +1,80 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.cuius.robotmind;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author terrence
+ */
+@Named(value = "schedules")
+@SessionScoped
+public class Schedules implements Serializable {
+
+    private String schedule1;
+    private String schedule2;
+    private String schedule3;
+    private String schedule4;
+    private String schedule5;
+    /**
+     * Creates a new instance of Schedules
+     */
+    public Schedules() {
+    }
+
+    public String getSchedule1() {
+        return schedule1;
+    }
+
+    public void setSchedule1(String schedule1) {
+        this.schedule1 = schedule1;
+    }
+
+    public String getSchedule2() {
+        return schedule2;
+    }
+
+    public void setSchedule2(String schedule2) {
+        this.schedule2 = schedule2;
+    }
+
+    public String getSchedule3() {
+        return schedule3;
+    }
+
+    public void setSchedule3(String schedule3) {
+        this.schedule3 = schedule3;
+    }
+
+    public String getSchedule4() {
+        return schedule4;
+    }
+
+    public void setSchedule4(String schedule4) {
+        this.schedule4 = schedule4;
+    }
+
+    public String getSchedule5() {
+        return schedule5;
+    }
+
+    public void setSchedule5(String schedule5) {
+        this.schedule5 = schedule5;
+    }
+    
+}
